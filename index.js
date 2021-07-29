a53855c38818baaefc28d34c117d3996c1e149bb
Tomas Carignano <tomascarignano98@gmail.com>
2021-07-29 15:23:47 -0300

    Got hold of the necessary DOM nodes and stored them in variables.

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 let player1Score = 0;
 let player2Score = 0;
 let player1Turn = true;
+
+// Variables to store references to the necessary DOM nodes
+const message = document.querySelector("#message");
+const player1Scoreboard = document.querySelector("#player1Scoreboard");
+const player2Scoreboard = document.querySelector("#player2Scoreboard");
+const player1Dice = document.querySelector("#player1Dice");
+const player2Dice = document.querySelector("#player2Dice");
+const rollBtn = document.querySelector("#rollBtn");
+const resetBtn = document.querySelector("#resetBtn");
