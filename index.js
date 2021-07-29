0b8e39f52ec0931a3a4c0ec836fcbe80edf0a3e0
Tomas Carignano <tomascarignano98@gmail.com>
2021-07-29 15:24:31 -0300

    Added the  game logic.

--- index.js
+++ index.js
@@ -11,3 +11,36 @@ const player1Dice = document.querySelector("#player1Dice");
 const player2Dice = document.querySelector("#player2Dice");
 const rollBtn = document.querySelector("#rollBtn");
 const resetBtn = document.querySelector("#resetBtn");
+
+// Game Logic
+
+rollBtn.addEventListener("click", () => {
+  const outcome = Math.floor(Math.random() * 6) + 1;
+
+  if (player1Turn) {
+    player1Score += outcome;
+    player1Turn = false;
+    player1Dice.textContent = outcome;
+  } else {
+    player2Score += outcome;
+    player1Turn = true;
+    player2Dice.textContent = outcome;
+  }
+
+  renderState();
+});
+
+function renderState() {
+  if (player1Turn) {
+    message.textContent = "Player 1 Turn";
+    player1Dice.classList.toggle("active");
+    player2Dice.classList.toggle("active");
+  } else {
+    message.textContent = "Player 2 Turn";
+    player1Dice.classList.toggle("active");
+    player2Dice.classList.toggle("active");
+  }
+
+  player1Scoreboard.textContent = player1Score;
+  player2Scoreboard.textContent = player2Score;
+}
